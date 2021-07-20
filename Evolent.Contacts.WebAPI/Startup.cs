cb107045d67818af211b2f448fb4afa89c006763
Nishant <nishakul2912@gmail.com>
2021-07-20 15:53:25 +0530

    Minor changes done for Contact List Service

--- Evolent.Contacts.WebAPI/Startup.cs
+++ Evolent.Contacts.WebAPI/Startup.cs
@@ -63,8 +63,6 @@ namespace Evolent.Contacts.WebAPI
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			
-
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
